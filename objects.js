0e5f9f5e71536721e6037cc4bcb314a5bfb26773
Joanne Lee <vernapii@gmail.com>
2018-03-15 18:59:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,6 @@
 var playlist = {playlist
 };
-var updatePlaylists(playlist, artistName, songTitle) {
-  //adds the 'artistName: songTitle' key value pair to 'playlist'
-  updatePlaylist(artistName,songTitle) = (artistName:songTitle);
+var updatePlaylists(artistName,songTitle) = {artistName:'',
+songTitle:''
 }
 
